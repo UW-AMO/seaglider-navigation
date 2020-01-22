a240cc9f3f2feca9a524c4519252385eeb22928c
Jake Stevens-Haas <jacob.stevens.haas@gmail.com>
2020-01-21 16:46:35 -0800

    Simulation now working

--- adcp/simulation.py
+++ adcp/simulation.py
@@ -10,24 +10,32 @@ import numpy as np
 import pandas as pd
 
 from . import matbuilder as mb
+from . import optimization as op
+from . import dataprep as dp
 
 def simulate(duration = pd.Timedelta('3 hours'), max_depth = 750, n_dives = 1,
              n_timepoints=1001, rho_c=.03, rho_t=.1, rho_a=1, rho_g=1, rho_r=1,
-             adcp_bins=4):
+             adcp_bins=4, seed=124):
     """Simulates the data recorded during a dive.
     
     Returns:
-        tuple of dictionaries.  The first element of the tuple is the dive
-        data, as would be loaded from adcp.dataprep.load_dive(dive_num).
-        The second element of the tuple is the ADCP data, as would be
-        loaded from adcp.dataprep.load_adcp(dive_num).
+        tuple of 2 dictionaries and a numpy vector.  The first element of
+        the tuple is the dive data, as would be loaded from
+        adcp.dataprep.load_dive(dive_num). The second element of the
+        tuple is the ADCP data, as would be loaded from
+        adcp.dataprep.load_adcp(dive_num).  The numpy vector is the true
+        simulated current and vehicle kinematics as it should be
+        constructed by a solver
+            [Vehicle, Current] -> [East, North] -> [v1 x1, v2, x2, ...]
     """
+    np.random.seed(seed)
     start_time = pd.Timestamp('2020-01-01')
     depths_down = np.arange(0,max_depth, max_depth / (n_timepoints//2))
     depths_up = max_depth + np.arange(.1,max_depth, max_depth / (n_timepoints//2))
     timepoints = np.linspace(start_time.value, (start_time+duration).value, 
                            n_timepoints)
     timepoints = pd.to_datetime(timepoints)
+    delta_t = timepoints[1:]-timepoints[:-1]
     v_ttw_n = 1 + .5 * np.random.normal(size = n_timepoints)
     v_ttw_e = .2 + .5 * np.random.normal(size = n_timepoints)
 
@@ -55,14 +63,12 @@ def simulate(duration = pd.Timedelta('3 hours'), max_depth = 750, n_dives = 1,
                                     ].depth.to_numpy().reshape((-1,1))
     down_adcp_depths = 12*np.random.random_sample((len(down_adcp_points), adcp_bins))
     down_adcp_depths = down_adcp_points - np.sort(down_adcp_depths, axis=1)
-    down_adcp_depths[down_adcp_depths < 0] = np.nan
 
     up_adcp_times = adcp_times[adcp_times > midpoint]
     up_adcp_points = depth_df.loc[up_adcp_times
                                   ].depth.to_numpy().reshape((-1,1))
     up_adcp_depths = 12*np.random.random_sample((len(up_adcp_points), adcp_bins))
     up_adcp_depths = up_adcp_points + np.sort(up_adcp_depths, axis=1)
-    up_adcp_depths[up_adcp_depths > 2* max_depth] = np.nan
 
     all_depths = np.concatenate((down_adcp_depths.flatten(), 
                                  up_adcp_depths.flatten(),
@@ -71,19 +77,29 @@ def simulate(duration = pd.Timedelta('3 hours'), max_depth = 750, n_dives = 1,
     all_depths.sort()
 
     ### Simulate current at all depths
-    Qc = mb.depth_Q(all_depths, rho_c)
     first_n = .5 * np.random.normal(size=1)
     first_e = .5 * np.random.normal(size=1)
-    delta_C_n = np.random.multivariate_normal(
-                        mean=np.zeros(len(all_depths)-1),
-                        cov = Qc.todense())
-    delta_C_e = np.random.multivariate_normal(
-                        mean=np.zeros(len(all_depths)-1),
-                        cov = Qc.todense())
-    cum_diff_n = np.cumsum(delta_C_n)
-    cum_diff_e = np.cumsum(delta_C_e)
-    current_n = np.concatenate((first_n, first_n+cum_diff_n))
-    current_e = np.concatenate((first_e, first_n+cum_diff_e))
+#   Truly principled method:
+#    Qc = mb.depth_Q(all_depths, rho_c)
+#    delta_C_n = np.random.multivariate_normal(
+#                        mean=np.zeros(len(all_depths)-1),
+#                        cov = Qc.todense())
+#    delta_C_e = np.random.multivariate_normal(
+#                        mean=np.zeros(len(all_depths)-1),
+#                        cov = Qc.todense())
+#    cum_diff_n = np.cumsum(delta_C_n)
+#    cum_diff_e = np.cumsum(delta_C_e)
+#    current_n = np.concatenate((first_n, first_n+cum_diff_n))
+#    current_e = np.concatenate((first_e, first_n+cum_diff_e))
+#   Shortcut method:
+    last_n = .5 * np.random.normal(size=1)
+    last_e = .5 * np.random.normal(size=1)
+    current_n = np.linspace(first_n[0], last_n[0], len(all_depths))
+    current_e = np.linspace(first_e[0], last_e[0], len(all_depths))
+    current_n = np.random.multivariate_normal(
+            mean = current_n, cov=rho_c*np.eye(len(current_n)))
+    current_e = np.random.multivariate_normal(
+            mean = current_e, cov=rho_c*np.eye(len(current_e)))
 
     curr_df = pd.DataFrame(data = np.hstack([current_e.reshape((-1,1)),
                                              current_n.reshape((-1,1))]), 
@@ -99,11 +115,10 @@ def simulate(duration = pd.Timedelta('3 hours'), max_depth = 750, n_dives = 1,
     v_df['ttw_n'] = v_ttw_n
     v_df['v_otg_n'] = v_df['curr_n']+v_df['ttw_n']
     v_df['v_otg_e'] = v_df['curr_e']+v_df['ttw_e']
-    delta_t = timepoints[1:]-timepoints[:-1]
     deltax_otg_n = (delta_t * v_df.v_otg_n.to_numpy()[1:]).total_seconds()
     deltax_otg_e = (delta_t * v_df.v_otg_e.to_numpy()[1:]).total_seconds()
-    v_df['dx_otg_n'] = np.nan
-    v_df['dx_otg_e'] = np.nan
+    v_df['dx_otg_n'] = 0
+    v_df['dx_otg_e'] = 0
     v_df = v_df.set_index('time')
     v_df.loc[timepoints[1:],'dx_otg_n'] = deltax_otg_n
     v_df.loc[timepoints[1:],'dx_otg_e'] = deltax_otg_e
@@ -128,8 +143,8 @@ def simulate(duration = pd.Timedelta('3 hours'), max_depth = 750, n_dives = 1,
     z_adcp_n = np.random.normal(loc = n_adcp_mean.astype(float), scale = rho_a)
     z_adcp_e = np.random.normal(loc = e_adcp_mean.astype(float), scale = rho_a)
 
-    dxn = deltax_otg_n[-1]
-    dxe = deltax_otg_e[-1]
+    dxn = sum(deltax_otg_n)
+    dxe = sum(deltax_otg_e)
     z_gps_n = np.random.normal(loc = dxn, scale = rho_g)
     z_gps_e = np.random.normal(loc = dxe, scale = rho_g)
 
@@ -165,4 +180,35 @@ def simulate(duration = pd.Timedelta('3 hours'), max_depth = 750, n_dives = 1,
                     down_adcp_depths, 2*max_depth - (up_adcp_depths))).T,
             'UV': (z_adcp_e+z_adcp_n*1j).T}
 
-    return ddat, adat
+
+    interp_depths = dp.depthpoints(adat, ddat)
+    ### Construct ideal optimization result
+    m = n_timepoints
+    n = len(interp_depths )
+    EV = op.ev_select(m, n)
+    NV = op.nv_select(m, n)
+    EC = op.ec_select(m, n)
+    NC = op.nc_select(m, n)
+    Vs = mb.v_select(m)
+    Xs = mb.x_select(m)
+
+
+    v_df['x_otg_n'] = v_df.dx_otg_n.cumsum()
+    v_df['x_otg_e'] = v_df.dx_otg_e.cumsum()
+    x = np.zeros(4*m+2*n)
+    x += EV.T @ Xs.T @ v_df.x_otg_e
+    x += EV.T @ Vs.T @ v_df.v_otg_e
+    x += NV.T @ Xs.T @ v_df.x_otg_n
+    x += NV.T @ Vs.T @ v_df.v_otg_n
+
+    #### Inference just uses depth in the dive to guess turnaround,
+    #### so need to interpolate current to the calculated ascending depths
+    curr_df = curr_df.append(pd.DataFrame(index = interp_depths,
+                                          columns = ['curr_e', 'curr_n']))
+    curr_df = curr_df.sort_index()
+    curr_df = curr_df.interpolate(method='index')
+    subset_df = curr_df.loc[~curr_df.index.duplicated()].reindex(interp_depths)
+    x += EC.T @ subset_df.curr_e
+    x += NC.T @ subset_df.curr_n
+
+    return ddat, adat, x
