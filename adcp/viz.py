fac4c13f91694a329e16058b07160b41f9fff88f
Jake Stevens-Haas <jacob.stevens.haas@gmail.com>
2020-04-13 10:37:44 -0700

    added ability to plot true solution to viz.py

--- adcp/viz.py
+++ adcp/viz.py
@@ -13,7 +13,7 @@ from adcp import matbuilder as mb
 cmap = plt.get_cmap("tab10")
 
 
-def current_depth_plot(x, adat, ddat, direction='north', mdat=None):
+def current_depth_plot(x, adat, ddat, direction='north', x_true=None, mdat=None):
     """Produce a current by depth plot, showing the current during 
     descending and ascending measurements.  Optionally truncate to just the
     region when mooring ground truth data is available.
@@ -22,14 +22,44 @@ def current_depth_plot(x, adat, ddat, direction='north', mdat=None):
     if direction.lower()=='both':
         plt.figure(figsize=[4,6])
         plt.subplot(1,2,1)
-        current_depth_plot(x, adat, ddat, direction='north', mdat=mdat)
+        current_depth_plot(x, adat, ddat, direction='north', x_true=x_true, mdat=mdat)
         plt.subplot(1,2,2)
-        current_depth_plot(x, adat, ddat, direction='east', mdat=mdat)
+        current_depth_plot(x, adat, ddat, direction='east', x_true=x_true, mdat=mdat)
         return
 #    plt.figure()
     ax=plt.gca()
     ax.set_title('')
-    
+    times = dp.timepoints(adat, ddat)
+    depths = dp.depthpoints(adat, ddat)    
+    m = len(times)
+    n = len(depths)
+    if direction.lower() in {'north','south'}:
+        currs = mb.nc_select(m, n) @ x
+    elif direction.lower() in {'east','west'}:
+        currs = mb.ec_select(m, n) @ x
+    depth_df = dp._depth_interpolator(times, ddat)
+    turnaround = depth_df.ascending.idxmax()
+    deepest = depth_df.loc[turnaround, 'depth']
+    sinking = currs[(depths < deepest) & (depths >0)] 
+    sinking_depths = depths[(depths < deepest) & (depths >0)] 
+    rising = currs[(depths > deepest) & (depths < deepest*2)] 
+    rising_depths = depths[(depths > deepest) & (depths < deepest*2)] 
+    ax.plot(sinking, sinking_depths, 'b-', label='Descending-Inferred')
+    ax.plot(rising, 2*deepest - rising_depths, 'r-', label='Ascending-Inferred')
+
+    # Add in true simulated profiles, if available
+    if x_true is not None:
+        if direction.lower() in {'north','south'}:
+            true_currs = mb.nc_select(m, n) @ x_true
+        elif direction.lower() in {'east','west'}:
+            true_currs = mb.ec_select(m, n) @ x_true
+        sinking_true = true_currs[(depths < deepest) & (depths >0)] 
+        rising_true = true_currs[(depths > deepest) & (depths < deepest*2)]
+        ax.plot(sinking_true, sinking_depths, 'b-',
+                label='Descending-True')
+        ax.plot(rising_true, 2*deepest - rising_depths, 'r-',
+                label='Ascending-True')
+        
     # Preprocess to get mooring data, if necessary:
     if mdat is not None:
         first_time = ddat['depth'].index.min()
@@ -40,29 +70,11 @@ def current_depth_plot(x, adat, ddat, direction='north', mdat=None):
         last_truth_time = mdat['time'][last_truth_idx]
         true_currs = mdat['u'] if direction.lower()=='north' else mdat['v']
         ax.plot(true_currs[first_truth_idx,:], mdat['depth'][first_truth_idx,:],
-                'bo', label='Descending-True')
+                'bo', label='Descending-Mooring')
         ax.plot(true_currs[last_truth_idx,:], mdat['depth'][last_truth_idx,:],
-                'ro', label='Ascending-True')
+                'ro', label='Ascending-Mooring')
 #        ax.legend(loc='lower left')
         
-    times = dp.timepoints(adat, ddat)
-    depths = dp.depthpoints(adat, ddat)    
-    m = len(times)
-    n = len(depths)
-    if direction.lower() in {'north','south'}:
-        currs = mb.nc_select(m, n) @ x
-    elif direction.lower() in {'east','west'}:
-        currs = mb.ec_select(m, n) @ x
-
-    depth_df = dp._depth_interpolator(times, ddat)
-    turnaround = depth_df.ascending.idxmax()
-    deepest = depth_df.loc[turnaround, 'depth']
-    sinking = currs[(depths < deepest) & (depths >0)] 
-    sinking_depths = depths[(depths < deepest) & (depths >0)] 
-    rising = currs[(depths > deepest) & (depths < deepest*2)] 
-    rising_depths = depths[(depths > deepest) & (depths < deepest*2)] 
-    ax.plot(sinking, sinking_depths, 'b-', label='Descending-Inferred')
-    ax.plot(rising, 2*deepest - rising_depths, 'r-', label='Ascending-Inferred')
 #    ax.legend()
     ax.invert_yaxis()
     ax.set_title(direction.title()+'erly Current')
@@ -78,7 +90,7 @@ def current_depth_plot(x, adat, ddat, direction='north', mdat=None):
     return ax
 
 def vehicle_speed_plot(solx, ddat, times, depths, direction='north', 
-                       x_sol=None, x0=None):
+                       x_sol=None, x_true=None, x0=None):
     plt.figure()
     m = len(times)
     n = len(depths)
@@ -94,16 +106,20 @@ def vehicle_speed_plot(solx, ddat, times, depths, direction='north',
     if x0 is not None:
         ln3 = plt.plot(times, Vs @ dirV @ x0, color=cmap(3), label='x0 Votg')
         lns.append(ln3[0])
+    if x_true is not None:
+        ln4 = plt.plot(times, Vs @ dirV @ x_true, color=cmap(3), label='Votg_true')
+        lns.append(ln4[0])
 #    ticks, labels = plt.xticks()
 #    inds = np.linspace(0,len(ticks),6, dtype=int, endpoint=False)
 #    plt.xticks(ticks[inds], np.array(labels)[inds])
     plt.twiny()
-    ln4 = plt.plot(mb.get_zttw(ddat).values/1e3, color=cmap(2), label='TTW measured')
-    lns.append(ln4[0])
+    ln5 = plt.plot(mb.get_zttw(ddat).values/1e3, color=cmap(2), label='TTW measured')
+    lns.append(ln5[0])
     labs = [l.get_label() for l in lns]
     plt.legend(lns, labs)
 
 def current_plot(solx, x_sol, adat, times, depths, direction='north'):
+    """ Deprecated """
     fig = plt.figure()
     m = len(times)
     n = len(depths)
@@ -119,7 +135,7 @@ def current_plot(solx, x_sol, adat, times, depths, direction='north'):
     return ax
 
 def vehicle_posit_plot(x, ddat, times, depths, x0=None, backsolve=None,
-                       dead_reckon=True):
+                       x_true=None, dead_reckon=True):
     m = len(times)
     n = len(depths)
     NV = mb.nv_select(m,n)
@@ -137,12 +153,15 @@ def vehicle_posit_plot(x, ddat, times, depths, x0=None, backsolve=None,
     if x0 is not None:
         ln3 = ax.plot(Xs @ EV @ x0/1000, Xs @ NV @ x0/1000, label='x0')
         lns.append(ln3[0])
+    if x_true is not None:
+        ln4 = ax.plot(Xs @ EV @ x_true/1000, Xs @ NV @ x_true/1000, label='X_true')
+        lns.append(ln4[0])
     if dead_reckon:
         df = dp.dead_reckon(ddat)
-        ln4 = ax.plot(df.x_dead/1000, df.y_dead/1000, label='Dead Reckoning')
-        ln5 = ax.plot(df.x_corr/1000, df.y_corr/1000, label='Reckoning, Corrected')
-        lns.append(ln4[0])
+        ln5 = ax.plot(df.x_dead/1000, df.y_dead/1000, label='Dead Reckoning')
+        ln6 = ax.plot(df.x_corr/1000, df.y_corr/1000, label='Reckoning, Corrected')
         lns.append(ln5[0])
+        lns.append(ln6[0])
     ax.legend()
     ax.set_xlabel('Easting (km)')
     ax.set_ylabel('Northing (km)')
