6a1fd16c84be7c8d404e93ef11a462cedb972ff0
Jake Stevens-Haas <jmsh@uw.edu>
2020-10-04 17:10:28 -0700

    Made parameter Search 1d work

--- test/parameter_search1d.py
+++ test/parameter_search1d.py
@@ -4,8 +4,11 @@ Created on Sun Apr  5 11:04:36 2020
 
 @author: 600301
 """
+# %% Imports
 import math
 from itertools import product, repeat
+from typing import Tuple, List
+import random
 
 import numpy as np
 from matplotlib import colors
@@ -17,14 +20,13 @@ from adcp import matbuilder as mb
 from adcp import optimization as op
 from adcp import viz
 
-# %% ...or simulate new data
-rho_t = 1e-1
-rho_a = 1e-1
-rho_g = 1e-1
-rho_c = 1e-3
-rho_v = 1e-3
-sim_rho_v = 1e-5
-sim_rho_c = 1e-5
+# %% Simulate new data
+rho_t = 1e-2
+rho_a = 1e-2
+rho_g = 1e-0
+
+sim_rho_v = 0
+sim_rho_c = 0
 sp = sim.SimParams(rho_t=rho_t, rho_a=rho_a, rho_g=rho_g, rho_v=sim_rho_v,
                     rho_c=sim_rho_c,
                     sigma_t=.4, sigma_c=.3, n_timepoints=2000,
@@ -35,8 +37,11 @@ depths = dp.depthpoints(adat, ddat)
 times = dp.timepoints(adat, ddat)
 
 # %% No Range
-rho_vs=rho_v * np.logspace(-8,0,14)
-rho_cs=rho_c * np.logspace(-8,0,14)
+rho_c = 1e5
+rho_v = 1e3
+n_steps = 11
+v_factors = np.logspace(0,0, n_steps)
+c_factors = np.logspace(-10,0, n_steps)
 rho_g=rho_g
 rho_t=rho_t
 rho_a=rho_a
@@ -49,13 +54,16 @@ print(f"""Solution method covariances:
     ADCP meawsurement: {rho_a}""")
 
 #Channel 1: position error or current error
-errmap = np.zeros((2, len(rho_vs), len(rho_cs)))
-paths = list(repeat(list(repeat(None, len(rho_cs))), len(rho_vs)))
+errmap = np.zeros((2,len(c_factors)))
+paths = []
 plt.figure()
-for ((i, rv),(j, rc)) in product(enumerate(rho_vs), enumerate(rho_cs)):
+for i, (v_factor, c_factor) in enumerate(zip(v_factors, c_factors)):
+    rc = c_factor * rho_c
+    rv = v_factor * rho_v
     prob=op.GliderProblem(ddat, adat, rho_v=rv, rho_c=rc, rho_g=rho_g,
                         rho_t=rho_t, rho_a=rho_a, rho_r=rho_r)
     # %%  Solve problem
+    print(f'solving for rho_v={rv} and rho_c={rc}.')
     seed = 3453
     x_sol, (NV, EV, NC, EC, Xs, Vs) = op.backsolve(prob)
 
@@ -63,45 +71,43 @@ for ((i, rv),(j, rc)) in product(enumerate(rho_vs), enumerate(rho_cs)):
     g = op.g(prob)
     h = op.h(prob)
     # %% Plotting
-
-    path_error = np.linalg.norm(((Xs @ NV) + (Xs @ EV)) @ (x_sol-x))**2
-    current_error = np.linalg.norm((EC + NC) @ (x_sol-x))**2
-    errmap[0,i,j] = path_error
-    errmap[1,i,j] = current_error
-    paths[i][j] = x_sol
-    # print(f"Scenario: rho_v = {rv} and rho_c = {rc}")
-    # print(f"\tPath error: {path_error}")
-    # print(f"\tCurrent error: {current_error}\n")
+    err = x_sol-x
+    path_error = (
+        np.linalg.norm(Xs @ NV @ err)**2 + 
+        np.linalg.norm(Xs @ EV @ err)**2
+    )
+    current_error = (
+        np.linalg.norm(EC @ err)**2 +
+        np.linalg.norm(NC @ err)**2
+    )
+    errmap[0,i] = path_error
+    errmap[1,i] = current_error
+    paths.append(x_sol)
 
 # %%
-def show_errmap(index: int=0, rho_vs: list=[], rho_cs:list=[]) -> None:
+def show_errmap(rho_vs: float, rho_cs: float) -> None:
     fig = plt.figure()
-    ax=fig.gca()#add_axes([.05,.05,.85,.9])
-    im = ax.imshow(errmap[index,:,:], norm=colors.LogNorm())
-    if index==0:
-        ax.set_title('Position Error')
-    else:
-        ax.set_title('Current Error')
-    ax.set_yticks(range(len(rho_vs)))
-    ax.set_xticks(range(len(rho_cs)))
-    ylabels = np.round(np.log(rho_vs)/math.log(10)).astype(int)
-    xlabels = np.round(np.log(rho_cs)/math.log(10)).astype(int)
-    ax.set_yticklabels(ylabels)
-    ax.set_xticklabels(xlabels)
-    ax.set_ylabel('rho_v (log scale)')
-    ax.set_xlabel('rho_c (log scale)')
-    cax = fig.add_axes([.95,.15,.05,.7])
-    ax.invert_yaxis()
-    fig.colorbar(im, cax=cax)
-    # plt.close()
-    # return fig
-show_errmap(0, rho_vs, rho_cs)
-show_errmap(1, rho_vs, rho_cs)
+    ax1 = fig.gca()
+    ln1 = ax1.loglog(rho_cs, errmap[0,:], c='blue', label='Position Error')
+    ax2 = ax1.twinx()
+    ln2 = ax2.loglog(rho_cs, errmap[1,:], c='red', label='Current Error')
+    ax1.set_title(f'Error for 1_D rho_v & rho_c search')
+    ax1.set_ylabel('Position Error')
+    ax2.set_ylabel('Current Error')
+    ax1.set_xlabel('rho_v\n rho_c')
+    ax1.set_xticklabels(
+        map(lambda tup: f'{tup[0]:.1e}\n{tup[1]:.1e}',
+        zip(rho_vs, rho_cs))
+    )
+    lns = ln1+ln2
+    labs = [l.get_label() for l in lns]
+    ax1.legend(lns, labs, loc=0)
+
+show_errmap(rho_v*v_factors, rho_c*c_factors)
 # %%
-i1, j1 = np.unravel_index(errmap[0,:,:].argmin(),  (len(rho_vs), len(rho_cs)))
-nav_x = paths[i1][j1]
-i2, j2 = np.unravel_index(errmap[1,:,:].argmin(),  (len(rho_vs), len(rho_cs)))
-curr_x = paths[i2][j2]
+f_nav, f_curr = tuple(errmap.argmin(axis=1))
+nav_x = paths[f_nav]
+curr_x = paths[f_curr]
 
 def plot_bundle(sol_x):
     ax1 = viz.vehicle_speed_plot(sol_x, ddat, times, depths, direction='both',
@@ -115,19 +121,23 @@ def plot_bundle(sol_x):
                                 x_true=x, dead_reckon=True)
 
 
-print(f'Best Navigational Solution: rho_v={rho_vs[i1]}, rho_c={rho_cs[j1]}')
+print(f'Best Navigational Solution: rho_v={rho_v*v_factors[f_nav]},'\
+        f' rho_c={rho_c*c_factors[f_nav]}')
 print(f"""For measurement errors:
     vehicle process: {sim_rho_v}
     current process:{sim_rho_c}
     GPS measurement: {rho_g}
     TTW measurement: {rho_t}
     ADCP meawsurement: {rho_a}
-Creates path error: {errmap[0, i1, j1]}
-and current error: {errmap[1, i1, j1]}""")
+Path error: {errmap[0, f_nav]}
+Current error: {errmap[1, f_nav]}""")
     
 plot_bundle(nav_x)
-if (i1 != i2) or (j1 != j2):
-    print(f'Best Current Solution: rho_v={rho_vs[i2]}, rho_c={rho_cs[j2]}')
+if f_nav != f_curr:
+    print(f'Best Current Solution: rho_v={rho_v*v_factors[f_curr]},'\
+            f' rho_c={rho_c*c_factors[f_curr]}')
+    print(f'Path error: {errmap[0, f_curr]}\n'\
+            f'Current error: {errmap[1, f_curr]}')
     plot_bundle(curr_x)
 else:
     print('... and it\'s also the best current solution')
