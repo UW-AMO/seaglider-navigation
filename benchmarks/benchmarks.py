fc5e1c81d2be040912a5d09c333b800c9744a927
Jake Stevens-Haas <jmsh@uw.edu>
2021-02-01 16:30:27 -0800

    deleteme

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,69 +1,67 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
 
-from adcp import simulation as sim
-from adcp import optimization as op
-
 
 class IntegrationSuite:
     def time_standard(self):
-        rho_t = 1e-3
-        rho_a = 1e-3
-        rho_g = 1e-1
+        3 + 3
+        # rho_t = 1e-3
+        # rho_a = 1e-3
+        # rho_g = 1e-1
 
-        sim_rho_v = 0
-        sim_rho_c = 0
-        sp = sim.SimParams(
-            rho_t=rho_t,
-            rho_a=rho_a,
-            rho_g=rho_g,
-            rho_v=sim_rho_v,
-            rho_c=sim_rho_c,
-            sigma_t=0.4,
-            sigma_c=0.3,
-            n_timepoints=2000,
-            measure_points={"gps": "endpoints", "ttw": 0.5, "range": 0.05},
-            vehicle_method="curved",
-            curr_method="curved",
-        )
-        ddat, adat, x, curr_df, v_df = sim.simulate(sp, verbose=True)
+        # sim_rho_v = 0
+        # sim_rho_c = 0
+        # sp = sim.SimParams(
+        #     rho_t=rho_t,
+        #     rho_a=rho_a,
+        #     rho_g=rho_g,
+        #     rho_v=sim_rho_v,
+        #     rho_c=sim_rho_c,
+        #     sigma_t=0.4,
+        #     sigma_c=0.3,
+        #     n_timepoints=2000,
+        #     measure_points={"gps": "endpoints", "ttw": 0.5, "range": 0.05},
+        #     vehicle_method="curved",
+        #     curr_method="curved",
+        # )
+        # ddat, adat, x, curr_df, v_df = sim.simulate(sp, verbose=True)
 
-        # %% No Range
-        rho_c = 1e0
-        rho_v = 1e0
-        rho_v = 1e-8
-        rho_c = 1e-8
-        rho_g = rho_g
-        rho_t = rho_t
-        rho_a = rho_a
-        rho_r = 0
-        print(
-            f"""Solution method covariances:
-        vehicle process: {rho_v}
-        current process:{rho_c}
-        GPS measurement: {rho_g}
-        TTW measurement: {rho_t}
-        ADCP meawsurement: {rho_a}"""
-        )
+        # # %% No Range
+        # rho_c = 1e0
+        # rho_v = 1e0
+        # rho_v = 1e-8
+        # rho_c = 1e-8
+        # rho_g = rho_g
+        # rho_t = rho_t
+        # rho_a = rho_a
+        # rho_r = 0
+        # print(
+        #     f"""Solution method covariances:
+        # vehicle process: {rho_v}
+        # current process:{rho_c}
+        # GPS measurement: {rho_g}
+        # TTW measurement: {rho_t}
+        # ADCP meawsurement: {rho_a}"""
+        # )
 
-        prob = op.GliderProblem(
-            ddat,
-            adat,
-            rho_v=rho_v,
-            rho_c=rho_c,
-            rho_g=rho_g,
-            rho_t=rho_t,
-            rho_a=rho_a,
-            rho_r=rho_r,
-            t_scale=1e3,
-            conditioner="tanh",
-            vehicle_vel="otg",
-            current_order=2,
-            vehicle_order=2,
-        )
+        # prob = op.GliderProblem(
+        #     ddat,
+        #     adat,
+        #     rho_v=rho_v,
+        #     rho_c=rho_c,
+        #     rho_g=rho_g,
+        #     rho_t=rho_t,
+        #     rho_a=rho_a,
+        #     rho_r=rho_r,
+        #     t_scale=1e3,
+        #     conditioner="tanh",
+        #     vehicle_vel="otg",
+        #     current_order=2,
+        #     vehicle_order=2,
+        # )
 
-        # %%  Solve problem
-        op.backsolve(prob)
+        # # %%  Solve problem
+        # op.backsolve(prob)
 
         # Xs = prob.Xs
         # EV = prob.EV
