8f7494d3d13f88d4c38a1394e5b63a3153598837
Jake Stevens-Haas <jacob.stevens.haas@gmail.com>
2021-04-19 21:09:20 -0700

    EXP: Added multi-gps trials

---
+++ adcp/exp/psearch2d_trials/trial9c.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+import adcp.exp
+from adcp.exp.psearch2d import ParameterSearch2D
+
+
+def __main__():
+    experiment = ParameterSearch2D(
+        variant="TTW Modeling, vehicle order=3, multi-first GPS point",
+        vehicle_vel="ttw",
+        vehicle_order=3,
+        gps_points="multi-first",
+        rho_vs=np.logspace(-10, -1, 11),
+        rho_cs=np.logspace(-10, -1, 11),
+    )
+    adcp.exp.run(experiment)
+
+
+if __name__ == "__main__":
+    __main__()
