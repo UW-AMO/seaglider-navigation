af97f0b55322b775e3460fdd480979e5ab0342db
Jake Stevens-Haas <jacob.stevens.haas@gmail.com>
2021-04-27 10:24:17 -0700

    TST: Add tests for high order smoothing w/t_scale=1

---
+++ adcp/exp/psearch2d_trials/trial15c.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+import adcp.exp
+from adcp.exp.psearch2d import ParameterSearch2D
+
+
+def __main__():
+    experiment = ParameterSearch2D(
+        variant="No Time Scaling, vehicle_order=3, current_order=3, "
+        "no conditioner, multi-first GPS only",
+        t_scale=1,
+        conditioner=None,
+        current_order=3,
+        vehicle_order=3,
+        gps_points="multi-first",
+        rho_vs=np.logspace(-10, -1, 11),
+        rho_cs=np.logspace(-10, -1, 11),
+    )
+    adcp.exp.run(experiment)
+
+
+if __name__ == "__main__":
+    __main__()
