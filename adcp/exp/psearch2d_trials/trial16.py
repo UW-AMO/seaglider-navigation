4aa91c32ec5fc554a2d11c81790296fbb3fd9ef2
Jake Stevens-Haas <jacob.stevens.haas@gmail.com>
2021-04-27 18:52:29 -0700

    EXP: Add all-methods-combined experiment

---
+++ adcp/exp/psearch2d_trials/trial16.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+import adcp.exp
+from adcp.exp.psearch2d import ParameterSearch2D
+
+
+def __main__():
+    experiment = ParameterSearch2D(
+        variant="No Time Scaling, vehicle_vel=TTW, vehicle order=3, "
+        "current_order=3",
+        t_scale=1,
+        vehicle_vel="ttw",
+        vehicle_order=3,
+        current_order=3,
+        rho_vs=np.logspace(-10, -1, 11),
+        rho_cs=np.logspace(-10, -1, 11),
+    )
+    adcp.exp.run(experiment)
+
+
+if __name__ == "__main__":
+    __main__()
