8b99193cea378cca1097f905fc438f8453f28f28
Jake Stevens-Haas <jacob.stevens.haas@gmail.com>
2021-06-23 22:27:05 -0700

    TST: add trial2c integration test

--- adcp/exp/tests/test_trials.py
+++ adcp/exp/tests/test_trials.py
@@ -1,6 +1,6 @@
 import pytest
 from adcp.exp.psearch2d import ParameterSearch2D
-from adcp.exp.psearch2d_trials import trial2
+from adcp.exp.psearch2d_trials import trial2, trial2c
 
 
 @pytest.mark.gitlocked
@@ -14,3 +14,16 @@ def test_default_integration_trial2a():
 
     assert 5176938 < results[0] < 5176939
     assert 3.6809045 < results[1] < 3.6809046
+
+
+@pytest.mark.gitlocked
+@pytest.mark.slow
+def test_default_integration_trial2c():
+    experiment = ParameterSearch2D(
+        **{**trial2c.prob_params, **{"rho_vs": [1e-3]}, **{"rho_cs": [1e-2]}},
+        **trial2c.sim_params,
+    )
+    results = experiment.run()["metrics"]
+
+    assert 2143393 < results[0] < 2143394
+    assert 15.602947 < results[1] < 15.602948
