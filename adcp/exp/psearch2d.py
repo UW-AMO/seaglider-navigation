a5ee1337a11da2258fe6c4b426cf6fcb9c508677
Jake Stevens-Haas <jacob.stevens.haas@gmail.com>
2021-06-23 20:24:30 -0700

    BUG: prevent NANs in errmap

--- adcp/exp/psearch2d.py
+++ adcp/exp/psearch2d.py
@@ -102,7 +102,10 @@ class ParameterSearch2D(Experiment):
             prob = op.GliderProblem(copyobj=self.prob, rho_v=rv, rho_c=rc)
 
             x_sol = op.backsolve(prob)
-
+            if np.isnan(x_sol.max()):  # too ill conditioned, couldn't solve
+                self.errmap[:, i, j] = [np.inf, np.inf]
+                self.paths.append(None)
+                continue
             legacy = mb.legacy_select(
                 prob.m,
                 prob.n,
