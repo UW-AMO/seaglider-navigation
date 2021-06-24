07c1d0ae6a3cdd2c2d6a86e2930e1cd00a2cfe3d
Jake Stevens-Haas <jacob.stevens.haas@gmail.com>
2021-06-24 14:06:43 -0700

    TST: Add more integration tests

--- adcp/exp/psearch2d_trials/trial16.py
+++ adcp/exp/psearch2d_trials/trial16.py
@@ -5,18 +5,19 @@ import numpy as np
 import adcp.exp
 from adcp.exp.psearch2d import ParameterSearch2D
 
+prob_params = {
+    "t_scale": 1,
+    "vehicle_vel": "ttw",
+    "current_order": 3,
+    "vehicle_order": 3,
+    "rho_vs": np.logspace(-10, 0, 11),
+    "rho_cs": np.logspace(-10, 0, 11),
+}
+sim_params = {}
+
 
 def __main__():
     experiment = ParameterSearch2D
-    prob_params = {
-        "t_scale": 1,
-        "vehicle_vel": "ttw",
-        "current_order": 3,
-        "vehicle_order": 3,
-        "rho_vs": np.logspace(-10, 0, 11),
-        "rho_cs": np.logspace(-10, 0, 11),
-    }
-    sim_params = {}
     adcp.exp.run(
         experiment,
         prob_params=prob_params,
