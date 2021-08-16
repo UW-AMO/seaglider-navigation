db86069a1c5aaafa2052cd419e88cacc20bc99fe
Jake Stevens-Haas <jacob.stevens.haas@gmail.com>
2021-08-16 15:11:51 -0700

    Added trials 18-20d and e.
    Also: changed name of Trial namedtuple arg to match with kwarg
    in adcp.exp.run().

--- adcp/exp/psearch2d_trials/__init__.py
+++ adcp/exp/psearch2d_trials/__init__.py
@@ -3,7 +3,7 @@ from collections import namedtuple
 
 import numpy as np
 
-from adcp.exp.psearch2d import ParameterSearch2D
+from adcp.exp.psearch2d import ParameterSearch2D, RigorousParameterSearch2D
 
 this_module = importlib.import_module(__name__)
 
@@ -19,7 +19,7 @@ def __getattr__(name):
     return imported
 
 
-Trial = namedtuple("Trial", ["experiment", "prob_params", "sim_params"])
+Trial = namedtuple("Trial", ["ex", "prob_params", "sim_params"])
 
 trial18a = Trial(
     ParameterSearch2D,
@@ -40,6 +40,28 @@ trial18c = Trial(
         "rho_cs": np.logspace(-10, 0, 11),
     },
     sim_params={"gps_points": "multi-first"},
+)
+trial18d = Trial(
+    RigorousParameterSearch2D,
+    prob_params={
+        "t_scale": 1,
+        "vehicle_vel": "otg-cov",
+        "rho_vs": np.logspace(-10, 0, 11),
+        "rho_cs": np.logspace(-10, 0, 11),
+    },
+    sim_params={
+        "sims": 20,
+    },
+)
+trial18e = Trial(
+    RigorousParameterSearch2D,
+    prob_params={
+        "t_scale": 1,
+        "vehicle_vel": "otg-cov",
+        "rho_vs": np.logspace(-10, 0, 11),
+        "rho_cs": np.logspace(-10, 0, 11),
+    },
+    sim_params={"sims": 20, "gps_points": "multi-first"},
 )
 trial19a = Trial(
     ParameterSearch2D,
@@ -62,6 +84,30 @@ trial19c = Trial(
         "rho_cs": np.logspace(-10, 0, 11),
     },
     sim_params={"gps_points": "multi-first"},
+)
+trial19d = Trial(
+    RigorousParameterSearch2D,
+    prob_params={
+        "t_scale": 1,
+        "vehicle_vel": "otg-cov",
+        "current_order": 3,
+        "rho_vs": np.logspace(-10, 0, 11),
+        "rho_cs": np.logspace(-10, 0, 11),
+    },
+    sim_params={
+        "sims": 20,
+    },
+)
+trial19e = Trial(
+    RigorousParameterSearch2D,
+    prob_params={
+        "t_scale": 1,
+        "vehicle_vel": "otg-cov",
+        "current_order": 3,
+        "rho_vs": np.logspace(-10, 0, 11),
+        "rho_cs": np.logspace(-10, 0, 11),
+    },
+    sim_params={"sims": 20, "gps_points": "multi-first"},
 )
 trial20a = Trial(
     ParameterSearch2D,
@@ -86,4 +132,30 @@ trial20c = Trial(
         "rho_cs": np.logspace(-10, 0, 11),
     },
     sim_params={"gps_points": "multi-first"},
+)
+trial20d = Trial(
+    RigorousParameterSearch2D,
+    prob_params={
+        "t_scale": 1,
+        "vehicle_vel": "otg-cov",
+        "vehicle_order": 3,
+        "current_order": 3,
+        "rho_vs": np.logspace(-10, 0, 11),
+        "rho_cs": np.logspace(-10, 0, 11),
+    },
+    sim_params={
+        "sims": 20,
+    },
+)
+trial20e = Trial(
+    RigorousParameterSearch2D,
+    prob_params={
+        "t_scale": 1,
+        "vehicle_vel": "otg-cov",
+        "vehicle_order": 3,
+        "current_order": 3,
+        "rho_vs": np.logspace(-10, 0, 11),
+        "rho_cs": np.logspace(-10, 0, 11),
+    },
+    sim_params={"sims": 20, "gps_points": "multi-first"},
 )
